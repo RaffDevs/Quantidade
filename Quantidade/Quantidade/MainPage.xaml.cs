062ba0cd0e5d6813d5633a418446abc0ee594089
RaffDevs <rveronezdeveloper@gmail.com>
2023-08-18 20:10:48 -0300

    first commit

---
+++ Quantidade/Quantidade/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Quantidade
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantity = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LbQuantidade.Text = quantity.ToString();
+            
+        }
+
+        private void Plus(object sender, EventArgs e)
+        {
+            quantity++;
+            LbQuantidade.Text = quantity.ToString();
+        }
+
+        private void Minus(object sender, EventArgs e)
+        {
+            if (quantity > 0)
+            {
+                quantity--;
+                LbQuantidade.Text = quantity.ToString();
+            }
+            
+        }
+    }
+}
